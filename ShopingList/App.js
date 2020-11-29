67b4e97d3da64d682f1fbd32f7f4a31d70e63d06
unknown <you@example.com>
2020-11-29 22:46:59 +0530

    add-items

--- ShopingList/App.js
+++ ShopingList/App.js
@@ -1,38 +1,48 @@
 import React, { useState } from 'react';
-import { SafeAreaView, Text, StyleSheet, FlatList } from 'react-native';
-import Header from './components/Header';
+import { SafeAreaView, Text, StyleSheet, FlatList, Alert } from 'react-native';
 import { v4 as uuidv4 } from 'uuid';
+import Header from './components/Header';
 import ListItems from './components/ListItems';
+import AddItem from './components/AddItem';
 
-  const App = () => {
-    const [items, setItems] = useState([
-      {
-        id: uuidv4(),
-        text: 'Milk',
-      },
-      {
-        id: uuidv4(),
-        text: 'Eggs',
-      },
-      {
-        id: uuidv4(),
-        text: 'Bread',
-      },
-      {
-        id: uuidv4(),
-        text: 'Juice',
-      },
-    ]);
-  
+const App = () => {
+  const [items, setItems] = useState('');
+
+
+
+
 const deleteItem = (id) => {
   setItems(prevItems => {
     return prevItems.filter(item => item.id != id);
   });
-}
+
+
+
+const addItem = text => {
+  if(!text) {
+    Alert.alert(
+      'No item entered',
+      'Please enter an item when adding to your shoping list',
+      [
+        {
+          text: 'OK',
+          style: 'cancel',
+        },
+      ],
+      {cancelable: true},
+    );
+  } else {
+    setItems(prevItems => {
+      return [{id: uuidv4(), text},...prevItems];
+    });
+  }
+
+};
 
   return (
     <SafeAreaView style={styles.container}>
      <Header title="Shopping List" />
+     <AddItem addItem={addItem}/>
      <FlatList data={items} 
      renderItem={({item}) => <ListItems item={item}
      deleteItem={deleteItem} />} />

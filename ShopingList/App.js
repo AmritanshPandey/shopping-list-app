f46d51c155537108effe1f839520a156fa85c376
unknown <you@example.com>
2020-11-29 20:58:25 +0530

    delete function

--- ShopingList/App.js
+++ ShopingList/App.js
@@ -1,25 +1,49 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Text, StyleSheet, FlatList } from 'react-native';
+import Header from './components/Header';
+import { v4 as uuidv4 } from 'uuid';
+import ListItems from './components/ListItems';
 
-const App = () => {
+  const App = () => {
+    const [items, setItems] = useState([
+      {
+        id: uuidv4(),
+        text: 'Milk',
+      },
+      {
+        id: uuidv4(),
+        text: 'Eggs',
+      },
+      {
+        id: uuidv4(),
+        text: 'Bread',
+      },
+      {
+        id: uuidv4(),
+        text: 'Juice',
+      },
+    ]);
+  
+const deleteItem = (id) => {
+  setItems(prevItems => {
+    return prevItems.filter(item => item.id != id);
+  });
+}
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+     <Header title="Shopping List" />
+     <FlatList data={items} 
+     renderItem={({item}) => <ListItems item={item}
+     deleteItem={deleteItem} />} />
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
-
-  text: {
-    color: 'darkslateblue',
-    fontSize: 30,
-  }
 
 })
 
